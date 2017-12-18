7926a71356f71d80ee8cb0884a5b11463d3bffd3
Jhonny Moreira <me.jhonnymoreira@gmail.com>
2017-12-18 06:09:33 -0200

    Update object reference in "dig" tests
    Updates "Obbie#dig" specs to handle Array indexes

--- tests/obbie/dig.test.js
+++ tests/obbie/dig.test.js
@@ -1,5 +1,5 @@
 import dig from '../../lib/obbie/dig'
-import object from '../mocks/filledObject'
+import object from '../mocks/object'
 
 describe('dig', () => {
   it('returns the value from a key sequence', () => {
@@ -8,6 +8,10 @@ describe('dig', () => {
 
   it('supports Array as key sequence argument', () => {
     expect(dig(object, ['a', 'b', 'c'])).toEqual(1)
+  })
+
+  it('supports Array indexes as part of the key sequence', () => {
+    expect(dig(object, 'a', 'e', 0, 'f')).toEqual(1)
   })
 
   it('returns "undefined" if last step of key sequence doesn\'t exist', () => {
